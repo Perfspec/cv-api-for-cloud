a1e74fc44fb401f1bb8e120269a6f5a3b6ace98a
Perfspec <lewisrwebb@gmail.com>
2018-12-10 15:17:30 +0000

    currently runs with spring cloud

--- src/main/java/com/qa/cvapi/rest/CVEndpoint.java
+++ src/main/java/com/qa/cvapi/rest/CVEndpoint.java
@@ -1,7 +1,6 @@
 package com.qa.cvapi.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +15,6 @@ import com.qa.cvapi.constants.Constants;
 import com.qa.cvapi.persistence.domain.CV;
 import com.qa.cvapi.service.CVService;
 
-@EnableEurekaClient
 @RestController
 @RequestMapping(path = "/cv")
 public class CVEndpoint {
